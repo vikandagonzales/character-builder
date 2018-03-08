577f52f2196d4e3996d92d6a6353da94cf6b0f03
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2018-03-08 11:28:44 -0800

    set up home page layout

--- index.php
+++ index.php
@@ -2,41 +2,106 @@
 
 <?php include 'nav.php';?>
 
-<section class="hero is-medium is-dark is-bold">
+<section id="character" class="hero is-medium is-dark is-bold">
   <div class="hero-body">
     <div class="container">
       <div class="columns">
         <div class="column is-8-desktop is-offset-2-desktop is-10-tablet is-offset-1-tablet">
-          <div class="content">
-            <h1 class="title has-text-centered">Title</h1>
-            <p class="has-text-justified">
-              Some text.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-<section class="hero is-medium is-light is-bold">
-  <div class="hero-body">
-    <div class="container">
-      <div class="columns">
-        <div class="column is-8-desktop is-offset-2-desktop is-10-tablet is-offset-1-tablet">
-          <div class="content has-text-centered">
-            <h1 class="title">Header</h1>
-            <h3 class="title">Sub-header</h3>
-            <a class="email button is-primary is-medium" href="#">
+          <h1 class="title has-text-centered">
+            Greetings, adventurer.
+          </h1>
+          <h1 class="subtitle has-text-centered">
+            Click on the cards below to start creating your character.
+          </h1>
+          <progress class="progress is-primary" value="30" max="100">30%</progress>
+          <div class="download has-text-centered">
+            <a class="button is-primary is-medium" href="#">
               <span class="icon is-small">
-                <i class="fas fa-envelope"></i>
+                <i class="fas fa-save"></i>
               </span>
-              <span>Some button</span>
+              <span class="is-hidden-mobile">Save File</span>
             </a>
-            <?php include 'social.php';?>
+            <a class="button is-danger is-medium" href="#">
+              <span class="icon is-small">
+                <i class="fas fa-download"></i>
+              </span>
+              <span class="is-hidden-mobile">Export PDF</span>
+            </a>
+            <a class="button is-light is-medium" href="#">
+              <span class="icon is-small">
+                <i class="fas fa-redo"></i>
+              </span>
+              <span class="is-hidden-mobile">Reset</span>
+            </a>
           </div>
         </div>
       </div>
+
+      <div class="columns">
+        <div class="column is-4">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-2by1">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content is-clearfix">
+                <p class="is-pulled-left">
+                  <span class="lnr-dna"></span>
+                  Race
+                </p>
+                <p class="is-pulled-right">
+                  0 / 2
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="column is-4">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-2by1">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content is-clearfix">
+                <p class="is-pulled-left">
+                  <span class="lnr-dagger"></span>
+                  Class
+                </p>
+                <p class="is-pulled-right">
+                  0 / 2
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="column is-4">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-2by1">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="content is-clearfix">
+                <p class="is-pulled-left">
+                  <span class="lnr-landscape"></span>
+                  Background
+                </p>
+                <p class="is-pulled-right">
+                  0 / 2
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </div>
 </section>
--- nav.php
+++ nav.php
@@ -1,59 +1,50 @@
 <nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="#home">
-      <img src="assets/brand/logo.svg" alt="Noosphere">
-    </a>
-    <div class="navbar-burger" data-target="navMenu">
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-  </div>
-  <div class="navbar-menu" id="navMenu">
-    <div class="navbar-end">
-      <a class="navbar-item underline" href="#about">
-        <span class="lnr lnr-dagger"></span> Character Builder
+  <div class="container">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="#home">
+        <img src="noosphere.svg" alt="Noosphere">
       </a>
+      <a class="navbar-item underline" href="#about">
+        <span class="lnr-dagger"></span>
+        <span class="is-hidden-touch">Character Builder</a>
+      </a>
+      <div class="navbar-burger" data-target="navMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
-    <div class="navbar-end is-hidden-touch">
-      <div class="navbar-item">
-        <div class="dropdown is-right is-hoverable">
-          <div class="dropdown-trigger">
-            <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
-              <img src="photo.jpg" alt="Vikanda">
-              <span>Vikanda</span>
-              <span class="icon is-small">
-                <i class="fas fa-angle-down" aria-hidden="true"></i>
-              </span>
-            </button>
-          </div>
-          <div class="dropdown-menu" id="dropdown-menu" role="menu">
-            <div class="dropdown-content">
-              <a href="#" class="dropdown-item">
-                Profile
-              </a>
-              <a href="#" class="dropdown-item">
-                Settings
-              </a>
-              <a href="#" class="dropdown-item">
-                Help
-              </a>
-              <hr class="dropdown-divider">
-              <a href="#" class="dropdown-item">
-                Characters
-              </a>
-              <a href="#" class="dropdown-item">
-                Campaigns
-              </a>
-              <hr class="dropdown-divider">
-              <a href="#" class="dropdown-item">
-                Log Out
-              </a>
-            </div>
+    <div class="navbar-menu" id="navMenu">
+      <div class="navbar-end">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link is-hidden-touch">
+            <img class="profile" src="photo.jpg" alt="Vikanda">
+            Vikanda
+          </a>
+          <div class="navbar-dropdown is-right">
+            <a class="navbar-item">
+              Profile
+            </a>
+            <a class="navbar-item">
+              Settings
+            </a>
+            <a class="navbar-item">
+              Help
+            </a>
+            <hr class="navbar-divider">
+            <a class="navbar-item">
+              Characters
+            </a>
+            <a class="navbar-item">
+              Campaigns
+            </a>
+            <hr class="navbar-divider">
+            <a class="navbar-item is-active">
+              Log Out
+            </a>
           </div>
         </div>
       </div>
     </div>
   </div>
-
 </nav>
